5e034c405cab5f58023bc922de8450a833a7df0b
Guido Giglioli <guido.tobugroup@gmail.com>
2026-06-22 16:19:56 +0200

    consegna esercizio

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
-///////////////////////// PROVA CON FETCH API /////////////////////////
+const unnList = document.querySelector('#lista');
 
-/* let arrResp = [];
 
-for (let i = 0; i < 3; i++) {
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then(response => response.json())
-      .then(data => {
-          // codice per far qualcosa con la risposta
-          console.log(data);
-          arrResp.push(data.response);
-          
-      })
-      .catch(error => {
-          // codice da eseguire in caso di errore
-        console.error(error);
-      });
-};
-
-console.log('QUESTO É arrResp:', arrResp); */
-
-///////////////////////// QUI CON AXIOS /////////////////////////
-
-axios.get('https://flynn.boolean.careers/exercises/api/random/boolean')
- .then(response => {
-   //codice da eseguire in caso di successo
-   const result = response.data;
- })
-.catch(error => {
-  // codice da eseguire in caso di errore
-  console.error(error)
-})
-.finally(() => {
-  // codice da eserguire a prescindere dall'esito
-  // (ad esempio si può fermare un loader!)
-})
+for (let i=0; i < 10; i++) {
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => {
+        const respData = response.data.response;
+        const htmlItem = `<li class="list-group-item">${respData}</li>`;
+        unnList.innerHTML += htmlItem;
+    });
+}
