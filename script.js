cf3c7b63194f9b3e205c7c7c1c840be7d99be477
Guido Giglioli <guido.tobugroup@gmail.com>
2026-06-22 16:34:01 +0200

    aggiunto bonus bottone

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const unnList = document.querySelector('#lista');
+const getMailBtn = document.getElementById('getMailBtn');
+const listHeader = '<li class="list-group-item active">Emails</li>'
+
+const getMails = () => {
+    for (let i=0; i < 10; i++) {
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            const respData = response.data.response;
+            const htmlItem = `<li class="list-group-item">${respData}</li>`;
+            unnList.innerHTML += htmlItem;
+        });
+    }
+};
 
 
-for (let i=0; i < 10; i++) {
-    axios
-    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response => {
-        const respData = response.data.response;
-        const htmlItem = `<li class="list-group-item">${respData}</li>`;
-        unnList.innerHTML += htmlItem;
-    });
-}
+getMails();
+
+getMailBtn.addEventListener('click', () => {
+    unnList.innerHTML = listHeader;
+    getMails();
+});
